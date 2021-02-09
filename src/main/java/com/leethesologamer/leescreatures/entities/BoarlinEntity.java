06fd8d63af31baad3edc420f530071843c0d4c61
LeeTheSoloGamer <78128647+LeeTheSoloGamer@users.noreply.github.com>
2021-02-08 20:01:51 -0500

    Update BoarlinEntity.java

--- src/main/java/com/leethesologamer/leescreatures/entities/BoarlinEntity.java
+++ src/main/java/com/leethesologamer/leescreatures/entities/BoarlinEntity.java
@@ -1,18 +1,23 @@
 package com.leethesologamer.leescreatures.entities;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.*;
-import net.minecraft.entity.effect.LightningBoltEntity;
-import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.passive.horse.AbstractHorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
@@ -26,19 +31,15 @@ import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
 import javax.annotation.Nullable;
-import java.util.function.Predicate;
 
-public class BoarlinEntity extends AnimalEntity implements IAnimatable {
+public class BoarlinEntity extends TameableEntity implements IAnimatable {
 
-    public static AnimationBuilder IDLE_ANIM = new AnimationBuilder().addAnimation("idle");
-    public static AnimationBuilder WALK_ANIM = new AnimationBuilder().addAnimation("walk");
+    private static final DataParameter<Boolean> ATTACKING = EntityDataManager.createKey(BoarlinEntity.class,
+            DataSerializers.BOOLEAN);
 
     private static final Ingredient TEMPTATION_ITEMS = Ingredient.fromItems(Items.WHEAT, Items.POTATO, Items.BEETROOT);
     private EatGrassGoal eatGrassGoal;
     private int exampleTimer;
-
-    private static final DataParameter<Boolean> IDLE = EntityDataManager.createKey(BoarlinEntity.class,
-            DataSerializers.BOOLEAN);
 
     public BoarlinEntity(EntityType<BoarlinEntity> entityType, World worldIn) {
         super(entityType, worldIn);
@@ -48,15 +49,15 @@ public class BoarlinEntity extends AnimalEntity implements IAnimatable {
     private AnimationFactory factory = new AnimationFactory(this);
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
-        if (!(event.getLimbSwingAmount() > -0.15F && event.getLimbSwingAmount() < 0.15F))
-        {
+        if (event.isMoving() && !this.isAggressive()) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("walking", true));
+            return PlayState.CONTINUE;
         }
-        else
-        {
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", true));
+            if (this.isAggressive()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("attacking", true));
+            return PlayState.CONTINUE;
         }
-        return PlayState.CONTINUE;
+        return PlayState.STOP;
     }
 
     @Override
@@ -65,22 +66,26 @@ public class BoarlinEntity extends AnimalEntity implements IAnimatable {
         this.eatGrassGoal = new EatGrassGoal(this);
         this.goalSelector.addGoal(0, new SwimGoal(this));
         this.goalSelector.addGoal(1, new BreedGoal(this, 1.0D));
-        this.goalSelector.addGoal(2, new FollowParentGoal(this, 1.1D));
+        this.goalSelector.addGoal(2, new FollowParentGoal(this, 0.3));
         this.goalSelector.addGoal(3, this.eatGrassGoal);
-        this.goalSelector.addGoal(4, new TemptGoal(this, 1.2D, false, TEMPTATION_ITEMS));
-        this.goalSelector.addGoal(5, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
-        this.goalSelector.addGoal(6, new LookAtGoal(this, PlayerEntity.class, 6.0F));
-        this.goalSelector.addGoal(7, new LookRandomlyGoal(this));
-        this.goalSelector.addGoal(8, new RandomWalkingGoal(this, 0.10D));
+        this.goalSelector.addGoal(4, new MeleeAttackGoal(this, 1.0D, true));
+        this.goalSelector.addGoal(5, new TemptGoal(this, 2.D, false, TEMPTATION_ITEMS));
+        this.goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 1.D));
+        this.goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+        this.applyEntityAI();
+    }
+    protected void applyEntityAI() {
         this.targetSelector.addGoal(1, new HurtByTargetGoal(this, PlayerEntity.class));
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal(this, CrystalWyvernEntity.class, 10, true, true, (Predicate)null));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, CrystalWyvernEntity.class, true));
     }
 
-    public static AttributeModifierMap.MutableAttribute func_234219_eI_() {
+
+    public static AttributeModifierMap.MutableAttribute registerAttributes() {
         return MobEntity.func_233666_p_()
                 .createMutableAttribute(Attributes.MAX_HEALTH, 45.0D)
-                .createMutableAttribute(Attributes.FOLLOW_RANGE, 20.0D)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.10D)
+                .createMutableAttribute(Attributes.FOLLOW_RANGE, 35.0D)
+                .createMutableAttribute(Attributes.MOVEMENT_SPEED, (double)0.23F)
                 .createMutableAttribute(Attributes.ATTACK_DAMAGE, 10.0D);
     }
     @Override
@@ -101,27 +106,18 @@ public class BoarlinEntity extends AnimalEntity implements IAnimatable {
 
     }
 
-    public void onStruckByLightning(LightningBoltEntity lightningBolt) {
-        this.setGlowing(true);
+    public int getMaxSpawnedInChunk() {
+        return 6;
     }
 
     @Override
     public void registerControllers(AnimationData animationData) {
-        animationData.addAnimationController(new AnimationController<BoarlinEntity>(this, "controller", 0,this::predicate));
+        animationData.addAnimationController(new AnimationController<>(this, "controller", 0,this::predicate));
     }
 
     @Override
     public AnimationFactory getFactory() {
         return this.factory;
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    public boolean isIdle() {
-        return this.dataManager.get(IDLE);
-    }
-
-    public void setIdle(boolean idle) {
-        this.dataManager.set(IDLE, idle);
     }
 
     @Nullable
@@ -133,14 +129,88 @@ public class BoarlinEntity extends AnimalEntity implements IAnimatable {
     @Override
     protected void registerData() {
         super.registerData();
-        this.dataManager.register(IDLE, false);
+        this.dataManager.register(ATTACKING, false);
     }
-    public boolean attackEntityAsMob(Entity p_70652_1_) {
-        boolean lvt_2_1_ = p_70652_1_.attackEntityFrom(DamageSource.causeMobDamage(this), (float)((int)this.getAttributeValue(Attributes.ATTACK_DAMAGE)));
-        if (lvt_2_1_) {
-            this.applyEnchantments(this, p_70652_1_);
+
+    public boolean isAttacking() {
+        return this.dataManager.get(ATTACKING);
+    }
+
+    public void setAttacking(boolean attacking) {
+        this.dataManager.set(ATTACKING,attacking);
+    }
+
+    public void writeAdditional(CompoundNBT compound) {
+        super.writeAdditional(compound);
+        compound.putBoolean("Attacking", this.isAttacking());
+
+    }
+
+    public void readAdditional(CompoundNBT compound) {
+        super.readAdditional(compound);
+        this.setAttacking(compound.getBoolean("Attacking"));
+    }
+
+    public boolean attackEntityFrom(DamageSource source, float amount) {
+        if (this.isInvulnerableTo(source)) {
+            return false;
+        }
+        else {
+            return super.attackEntityFrom(source, amount);
+        }
+    }
+
+    public boolean attackEntityAsMob(Entity entityIn) {
+        boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), (float)((int)this.getAttributeValue(Attributes.ATTACK_DAMAGE)));
+        if (flag) {
+            this.applyEnchantments(this, entityIn);
         }
 
-        return lvt_2_1_;
+        return flag;
+    }
+    public boolean shouldAttackEntity(LivingEntity target, LivingEntity owner) {
+        if (!(target instanceof CrystalWyvernEntity)) {
+            if (target instanceof BoarlinEntity) {
+                BoarlinEntity boarlinEntity = (BoarlinEntity)target;
+                return !boarlinEntity.isTamed() || boarlinEntity.getOwner() != owner;
+            } else if (target instanceof PlayerEntity && owner instanceof PlayerEntity && !((PlayerEntity)owner).canAttackPlayer((PlayerEntity)target)) {
+                return false;
+            } else if (target instanceof AbstractHorseEntity && ((AbstractHorseEntity)target).isTame()) {
+                return false;
+            } else {
+                return !(target instanceof TameableEntity) || !((TameableEntity)target).isTamed();
+            }
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void setTamedBy(PlayerEntity player) {
+        super.setTamedBy(player);
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.ENTITY_PIG_AMBIENT;
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource damageSourceIn) {
+        return SoundEvents.ENTITY_PIG_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ENTITY_PIG_DEATH;
+    }
+
+    protected SoundEvent getStepSound() {
+        return SoundEvents.ENTITY_PIG_STEP;
+    }
+
+    @Override
+    protected void playStepSound(BlockPos pos, BlockState blockIn) {
+        this.playSound(this.getStepSound(), 0.15F, 1.0F);
     }
 }
