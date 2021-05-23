8557cde1d322ec63bd7ed751c7a07d8903b8d757
Yusuf Ismail <yusufgamer222@gmail.com>
2021-05-23 16:48:10 +0100

    Create DataGenerators.java

---
+++ src/main/java/com/leethesologamer/leescreatures/data/DataGenerators.java
@@ -0,0 +1,19 @@
+package com.leethesologamer.leescreatures.data;
+
+import com.leethesologamer.leescreatures.LeesCreatures;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(modid = LeesCreatures.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public final class DataGenerators {
+    private DataGenerators() {
+    }
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator gen = event.getGenerator();
+
+    }
+}
