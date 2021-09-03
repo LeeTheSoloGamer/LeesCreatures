868fd899f65cac08626cddacea8c9b8c64bcf59e
LeeTheSoloGamer <78128647+LeeTheSoloGamer@users.noreply.github.com>
2021-09-03 11:51:07 -0500

    Add files via upload

--- src/main/java/com/leethesologamer/leescreatures/util/ClientEventBusSubscriber.java
+++ src/main/java/com/leethesologamer/leescreatures/util/ClientEventBusSubscriber.java
@@ -2,6 +2,14 @@ package com.leethesologamer.leescreatures.util;
 
 import com.leethesologamer.leescreatures.LeesCreatures;
 import com.leethesologamer.leescreatures.client.entity.render.CrystalWyvernEntityRender;
+import com.leethesologamer.leescreatures.client.entity.render.BoarlinEntityRender;
+import com.leethesologamer.leescreatures.client.entity.render.BeastDogRender;
+import com.leethesologamer.leescreatures.client.entity.render.SouleuronEntityRender;
+import com.leethesologamer.leescreatures.client.entity.render.JungleSerpentEntityRender;
+import com.leethesologamer.leescreatures.client.entity.render.CrestedCrikestreakerRender;
+import com.leethesologamer.leescreatures.client.entity.render.AdderBackEntityRender;
+import com.leethesologamer.leescreatures.client.entity.render.FlorretEntityRender;
+import com.leethesologamer.leescreatures.client.entity.render.CosmicWhaleEntityRender;
 import com.leethesologamer.leescreatures.init.ModEntityTypes;
 
 import net.minecraftforge.api.distmarker.Dist;
@@ -16,17 +24,13 @@ public class ClientEventBusSubscriber {
     @SubscribeEvent
     public static void clientSetup(FMLClientSetupEvent event) {
     	 RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.CRYSTAL_WYVERN_ENTITY.get(), CrystalWyvernEntityRender::new);
-    	
-    	/*
-        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.BOARLIN_ENTITY.get(), BoarlinEntityRender::new);
-        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.SOULEURON_ENTITY.get(), SouleuronEntityRender::new);
-        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.CRYSTAL_WYVERN_ENTITY.get(), CrystalWyvernEntityRender::new);
-        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.JUNGLE_SERPENT_ENTITY.get(), JungleSerpentEntityRender::new);
-        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.BEAST_DOG_ENTITY.get(), BeastDogRender::new);
-        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.CRESTED_CRIKESTREAKER_ENTITY.get(), CrestedCrikestreakerRender::new);
-        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.ADDER_BACK_ENTITY.get(), AdderBackEntityRender::new);
-        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.FLORRET_ENTITY.get(), FlorretEntityRender::new);
-        RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.COSMIC_WHALE_ENTITY.get(), CosmicWhaleEntityRender::new);
-    	 */
+    	 RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.BOARLIN_ENTITY.get(), BoarlinEntityRender::new);
+    	 RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.SOULEURON_ENTITY.get(), SouleuronEntityRender::new);
+    	 RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.JUNGLE_SERPENT_ENTITY.get(), JungleSerpentEntityRender::new);
+         RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.BEAST_DOG_ENTITY.get(), BeastDogRender::new);
+         RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.CRESTED_CRIKESTREAKER_ENTITY.get(), CrestedCrikestreakerRender::new);
+         RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.ADDER_BACK_ENTITY.get(), AdderBackEntityRender::new);
+         RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.FLORRET_ENTITY.get(), FlorretEntityRender::new);
+         RenderingRegistry.registerEntityRenderingHandler(ModEntityTypes.COSMIC_WHALE_ENTITY.get(), CosmicWhaleEntityRender::new);
     }
 }
